c209e7e483334c16f61dbcb2bfa362eb8733a272
Vilma Hernandez <vilmaglynn@hotmail.com>
2023-12-16 13:58:24 -0600

    added prompt for password details

--- starter/script.js
+++ starter/script.js
@@ -91,16 +91,44 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+  let passwordLength = "";
+
+  do {
+    passwordLength = prompt("Enter the desired length of the password. Password MUST be at least 8 characters but no more than 128.");
+  
+    // Check if the input is empty
+    if (passwordLength === "" || passwordLength === null) {
+      alert("Password length cannot be empty. Please enter a valid length.");
+    }
+  
+    // Check if the input is not a number or if it's outside the range
+  } while (isNaN(passwordLength) || parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128 || passwordLength === "" || passwordLength === null);
+  
+  
+let includeLowercase, includeUppercase, includeNumbers, includeSpecialChars;
+
+//===============================
+do {
+  includeLowercase = confirm("Include lowercase letters?");
+  includeUppercase = confirm("Include uppercase letters?");
+  includeNumbers = confirm("Include numbers?");
+  includeSpecialChars = confirm("Include special characters?");
+
+  if (!(includeLowercase || includeUppercase || includeNumbers || includeSpecialChars)) {
+    alert("Please select at least one option.");
+  }
+} while (!(includeLowercase || includeUppercase || includeNumbers || includeSpecialChars));
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  let randomItem = arr[Math.floor(Math.random()*arr.length)];
 }
 
 // Function to generate password with user input
 function generatePassword() {
 
+  
 }
 
 // Get references to the #generate element
@@ -116,3 +144,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+getPasswordOptions() 
+getRandom(arr)
